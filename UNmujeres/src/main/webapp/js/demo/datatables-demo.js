c0d628854d0e789c150fbda284d86bc8f63bb019
Luis <a20221203@pucp.edu.pe>
2025-07-01 22:48:14 -0500

    Update datatables-demo.js

--- UNmujeres/src/main/webapp/js/demo/datatables-demo.js
+++ UNmujeres/src/main/webapp/js/demo/datatables-demo.js
@@ -1,36 +1,40 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
     $('#dataTable').DataTable({
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+        "order": [], // Deshabilita el orden inicial si lo deseas, o ajusta según necesites
+        "pageLength": 10, // Número de filas por página por defecto
+        "language": { // Configuración del idioma
+            "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json" // URL para español
         }
     });
 });
 
 $(document).ready(function() {
     $('#tablaAsig').DataTable({
-        //paging: true,       // Habilita la paginación
-        //ordering: true,     // Permite el ordenamiento por columnas
-        //searching: true,    // Agrega la funcionalidad de búsqueda global
-        //info: true,         // Muestra información de la tabla (por ejemplo, "Mostrando X de Y entradas")
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+        "order": [], // Deshabilita el orden inicial si lo deseas, o ajusta según necesites
+        "pageLength": 10, // Número de filas por página por defecto
+        "language": { // Configuración del idioma
+            "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json" // URL para español
         }
     });
 });
 
 $(document).ready(function() {
     $('#tablaDrafts').DataTable({
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+        "order": [], // Deshabilita el orden inicial si lo deseas, o ajusta según necesites
+        "pageLength": 10, // Número de filas por página por defecto
+        "language": { // Configuración del idioma
+            "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json" // URL para español
         }
     });
 });
 
 $(document).ready(function() {
     $('#tablaRecords').DataTable({
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+        "order": [], // Deshabilita el orden inicial si lo deseas, o ajusta según necesites
+        "pageLength": 10, // Número de filas por página por defecto
+        "language": { // Configuración del idioma
+            "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json" // URL para español
         }
     });
 });
